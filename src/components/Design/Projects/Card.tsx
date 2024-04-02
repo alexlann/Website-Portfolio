6c9e4dd4a10668c3071fcd32babc4dff2bcd19c2
alexlann <lannoy.alexander@gmail.com>
2024-04-02 22:46:59 +0200

    fix card link

--- src/components/Design/Projects/Card.tsx
+++ src/components/Design/Projects/Card.tsx
@@ -3,14 +3,13 @@ import { ReactElement } from "react";
 export default function Card({ link, children } : { link?: string, children: ReactElement }) {
   return (
     <div className="relative w-full h-full bg-light-green rounded">
-      {/* TODO: met button proberen om link optioneel te maken? */}
-      {/* <a href={ link || "" } className={ link ? "pointer-events-none" : "" }> */}
+      <a target="_blank" rel="noreferrer" href={ link || "" } className={ !link ? "pointer-events-none" : "" }>
         <div className="bg-white relative right-3 bottom-3 h-full border border-light-green border-2 rounded group">
           <div className="py-2 px-3 h-full">
             { children }
           </div>
         </div>
-      {/* </a> */}
+      </a>
     </div>
   )
 }
